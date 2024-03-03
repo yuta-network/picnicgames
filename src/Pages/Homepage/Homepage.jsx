62a4adc9d9258bdd35d4c085a283c4c94d1f6127
Trần Công Tấn <88374323+yuta-network@users.noreply.github.com>
2024-03-03 17:50:23 +0700

    fix top edge of footer

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -71,7 +71,90 @@ function Homepage() {
           <div className="col-md-2"></div>
         </div>
       </div>
-      <div className="featured-games-section"></div>
+      <div className="featured-games-section container-fluid">
+        <div className="container">
+          <div className="featured-games-title">
+            <span>Featured Games</span>
+          </div>
+          <div className="featured-games-card-layout">
+            <div class="row row-cols-1 row-cols-md-4 g-4">
+              <div class="col">
+                <div class="card h-100">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-body-secondary">
+                      Last updated 3 mins ago
+                    </small>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card h-100">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      This card has supporting text below as a natural lead-in
+                      to additional content.
+                    </p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-body-secondary">
+                      Last updated 3 mins ago
+                    </small>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card h-100">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This card has even
+                      longer content than the first to show that equal height
+                      action.
+                    </p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-body-secondary">
+                      Last updated 3 mins ago
+                    </small>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card h-100">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This card has even
+                      longer content than the first to show that equal height
+                      action.
+                    </p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-body-secondary">
+                      Last updated 3 mins ago
+                    </small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
